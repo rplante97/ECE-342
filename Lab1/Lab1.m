7d4da89abca0c9e4f49df076d247f791e1870bcc
patrick24r <31832331+patrick24r@users.noreply.github.com>
2017-09-11 19:32:54 -0400

    Lab 1 Simulation complete
    Simulation files and matlab figure for Lab 1 simulations

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,5 +1,5 @@
-%% Sample Matlab m-file for ECE342
-% ECE 214 Lab 9 - Astable Multivibrator
+%% Matlab m-file for ECE342
+% ECE 342 Lab 1 - TZA
 
 %% Set variables and filenames
 % add the ngspice matlab toolbox to the path
@@ -9,24 +9,11 @@ format long;
 clear variables;
 % set the hspc filename
 hspc_filename = sprintf('test.hspc');
-% set the digilent csv filename
-digilent_filename = sprintf('test.csv');
-
-%% Set component parameters
-capacitor_1 = 1.35e-9;
-capacitor_2 = 1.35e-9;
-hspc_set_param('cap1', capacitor_1, hspc_filename);
-hspc_set_param('cap2', capacitor_2, hspc_filename);
-
-rdown = 30e3;
-rup = 60e3;
-rx = 1e3;
-hspc_set_param('ru', rup, hspc_filename);
-hspc_set_param('rd', rdown, hspc_filename);
-hspc_set_param('rx', rx, hspc_filename);
+hspc_filename_2 = sprintf('test2.hspc');
 
 %% Set parameters for transient simulation and write to hspc file
 hspc_addline('.tran .1u 1m', hspc_filename);
+hspc_addline('.ac dec 200 0 1e6', hspc_filename_2);
 
 %% Run ngspice
 ngsim(hspc_filename);
@@ -35,20 +22,21 @@ ngsim(hspc_filename);
 simdata = loadsig('simrun.raw');
 sim_time = evalsig(simdata, 'TIME');
 sim_Vout1 = evalsig(simdata,'vout');
-%sim_Vout2 = evalsig(simdata,'xi0_vout2');
 
-%% Load Digilent results and extract time and channel1 and channel2
-%measdata = csvread(digilent_filename, 6);
-% meas_time = measdata(:,1);
-% meas_v1 = measdata(:,2);
-% meas_v2 = measdata(:,3);
+%% Run AC simulation
+ngsim(hspc_filename_2);
+simdata = loadsig('simrun.raw');
+sim_frequency = evalsig(simdata, 'FREQUENCY');
+sim_Vout2 = evalsig(simdata,'vout');
+sim_Vin2 = evalsig(simdata,'vin');
+sim_Vout2 = 20 .* log10(abs(sim_Vout2) ./ abs(sim_Vin2));
 
 %% Plot Vout as a function of time
 % define font size (fs) and linewidth (lw)
 fs = 16;
 lw = 1.5;
 % set figure size and location
-FigHandle = figure('Name', 'Oscillator Output', 'Position', [200, 75, 850, 600]);
+FigHandle = figure('Name', 'Amplifier Output', 'Position', [200, 75, 850, 600]);
 
 %% First subplot
 subplot(2,1,1);
@@ -63,26 +51,27 @@ set(gca, 'fontsize', fs);
 ylabel('Voltage (V)', 'fontsize', fs);
 xlabel('Time (ms)', 'fontsize', fs);
 % title
-title('Astable Multivibrator')
+title('Time Domain')
 % legend
-legend('Simulated Output', 'Measured Output', 'location', 'northwest');
-% set axis
-%axis([0, .2, 0, 10]);
+legend('Simulated Output');
+% set axis limits
+axis([0, 1, -5, 5]);
 
 %% second subplot
 subplot(2,1,2)
-plot(sim_time*1e3, sim_Vout2, meas_time*1e3+0.38, meas_v1, 'linewidth', lw);
-%axis([0,.5,-4,10]);
+semilogx(sim_frequency, sim_Vout2, 'linewidth', lw);
 % add grid
 grid on;
 % increase font size
 set(gca, 'fontsize', fs);
 % y-axis and x-axis labels
-ylabel('Voltage (V)', 'fontsize', fs);
-xlabel('Time (ms)', 'fontsize', fs);
+ylabel('Gain (dB)', 'fontsize', fs);
+xlabel('Frequency (Hz)', 'fontsize', fs);
+% title
+title('Frequency Domain')
 % legend
-legend('Siulated Output', 'Measured Output', 'location', 'northwest');
+legend('Simulated Output');
 % set axis limits
-%axis([0, .1, 0, 20]);
+axis([0, 1e6, 0, 120]);
 
 %% end of M file
--- Lab1/simrun.sp
+++ Lab1/simrun.sp
@@ -1,13 +1,13 @@
 ***** Spice Netlist for Cell 'Lab1' *****
 
 ************** Module Lab1 **************
-xi0 gnd n0 n1 n2 vout lf347 
-v0 gnd n2 dc='12' ac='0' 
 v1 n1 gnd dc='12' ac='0' 
-c0 gnd n0 4p
-r0 n0 vout 180k noisy=0
-c1 n0 vout 22p
-i0 gnd n0 dc 0a ac 0 sin('0a' '1n' '20k' '.1us') 
+xi0 gnd vin n1 n0 vout lf347 
+v0 n0 gnd dc='-12' ac='0' 
+c0 gnd vin 4p
+i0 vin gnd dc 0a ac 1a sin('0a' '10u' '20k' '0s') 
+c1 vin vout 10p
+r0 vin vout 400k noisy=0
 
 
 
@@ -16,8 +16,7 @@ i0 gnd n0 dc 0a ac 0 sin('0a' '1n' '20k' '.1us')
 .include '../../../SpiceModels/ECE214_models.mod'
 .include '../../../SpiceModels/ECE342_models.mod'
 .temp 27
-.tran .1u 1m
-.ac .1u 1m
+.ac dec 200 0 1e6
 .global gnd
 .options post=1 delmax=5p relv=1e-6 reli=1e-6 relmos=1e-6 method=gear
 
