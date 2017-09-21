a7a768ee2ca7484d5f0547d3c11e31f75ef4ff93
patrick24r <31832331+patrick24r@users.noreply.github.com>
2017-09-21 17:02:25 -0400

    Measured Results
    Matlab graphing measured results

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -11,9 +11,30 @@ clear variables;
 hspc_filename = sprintf('test.hspc');
 hspc_filename_2 = sprintf('test2.hspc');
 
+%% Read digilent csv files
+Lab1FFT = sprintf('Lab1ScopeFFT.csv');
+Lab1Freq = sprintf('Lab1Network.csv');
+Lab1Out = sprintf('Lab1Scope.csv');
+
+% read Vout in
+VoutData = csvread(Lab1Out, 9);
+VoutTime = VoutData(:,1);
+VoutVout = VoutData(:,2);
+
+% read FFT data in
+FFTdata = csvread(Lab1FFT, 6);
+FFTfreq = FFTdata(:,1);
+FFTdB = FFTdata(:,2);
+
+% read frequency response data in
+FreqR = csvread(Lab1Freq, 6);
+FreqRfreq = FreqR(:,1);
+dBGain = FreqR(:,2);
+phase = FreqR(:,3);
+
 %% Set parameters for transient simulation and write to hspc file
 hspc_addline('.tran .1u 1m', hspc_filename);
-hspc_addline('.ac dec 200 0 1e6', hspc_filename_2);
+hspc_addline('.ac dec 200 0 1e7', hspc_filename_2);
 
 %% Run ngspice
 ngsim(hspc_filename);
@@ -29,20 +50,19 @@ simdata = loadsig('simrun.raw');
 sim_frequency = evalsig(simdata, 'FREQUENCY');
 sim_Vout2 = evalsig(simdata,'vout');
 sim_Vin2 = evalsig(simdata,'vin');
-sim_Vout2 = 20 .* log10(abs(sim_Vout2) ./ abs(sim_Vin2));
+sim_Vout2 = 20 .* log10(abs(sim_Vout2));
 
 %% Plot Vout as a function of time
 % define font size (fs) and linewidth (lw)
 fs = 16;
 lw = 1.5;
 % set figure size and location
-FigHandle = figure('Name', 'Amplifier Output', 'Position', [200, 75, 850, 600]);
+FigHandle = figure('Name', 'Simulated Amplifier Output', 'Position', [200, 75, 850, 600]);
 
 %% First subplot
 subplot(2,1,1);
 
 plot(sim_time.*1e3,  sim_Vout1, 'linewidth',lw);
-%axis([0, .5, -2, 10]);
 % add grid
 grid on;
 % increase font size
@@ -72,6 +92,77 @@ title('Frequency Domain')
 % legend
 legend('Simulated Output');
 % set axis limits
-axis([0, 1e6, 0, 120]);
+axis([0, 1e7, 0, 120]);
 
+FigHandle2 = figure('Name', 'Measured Amplifier Output', 'Position', [200, 75, 850, 600]);
+
+%% First Subplot
+subplot(2,2,1)
+plot(VoutTime .* 1e3, VoutVout, 'linewidth', lw)
+% add grid
+grid on;
+% increase font size
+set(gca, 'fontsize', fs);
+% y-axis and x-axis labels
+ylabel('Vout (V)', 'fontsize', fs);
+xlabel('Time (ms)', 'fontsize', fs);
+% title
+title('TZA Output')
+% legend
+legend('Measured Output');
+% set axis limits
+axis([0, 1, -5, 7]);
+
+%% Second Subplot
+subplot(2,2,2)
+plot(FFTfreq ./ 1e3, FFTdB, 'linewidth', lw)
+% add grid
+grid on;
+% increase font size
+set(gca, 'fontsize', fs);
+% y-axis and x-axis labels
+ylabel('Amplitude (dB)', 'fontsize', fs);
+xlabel('Frequency (kHz)', 'fontsize', fs);
+% title
+title('TZA Output FFT')
+% legend
+legend('Measured Output');
+% set axis limits
+xlim = 2e2;
+axis([0, xlim, -120, 20]);
+xticks(linspace(20, xlim, xlim/20));
+
+%% Third Subplot
+subplot(2,2,3)
+semilogx(FreqRfreq, dBGain, 'linewidth', lw)
+% add grid
+grid on;
+% increase font size
+set(gca, 'fontsize', fs);
+% y-axis and x-axis labels
+ylabel('Gain (dB)', 'fontsize', fs);
+xlabel('Frequency (Hz)', 'fontsize', fs);
+% title
+title('TZA Frequency Response')
+% legend
+legend('Measured Output');
+% set axis limits
+axis([1e3, 1e6, -40, 15]);
+
+%% Fourth Subplot
+subplot(2,2,4)
+semilogx(FreqRfreq, phase, 'linewidth', lw)
+% add grid
+grid on;
+% increase font size
+set(gca, 'fontsize', fs);
+% y-axis and x-axis labels
+ylabel('Phase Shift (Degrees)', 'fontsize', fs);
+xlabel('Frequency (Hz)', 'fontsize', fs);
+% title
+title('TZA Frequency Response')
+% legend
+legend('Measured Output');
+% set axis limits
+axis([1e3, 1e6, -100, 200]);
 %% end of M file
--- Lab1/simrun.sp
+++ Lab1/simrun.sp
@@ -1,13 +1,13 @@
 ***** Spice Netlist for Cell 'Lab1' *****
 
 ************** Module Lab1 **************
-v1 n1 gnd dc='12' ac='0' 
-xi0 gnd vin n1 n0 vout lf347 
-v0 n0 gnd dc='-12' ac='0' 
+v1 n0 gnd dc='12' ac='0' 
+v0 n1 gnd dc='-12' ac='0' 
 c0 gnd vin 4p
 i0 vin gnd dc 0a ac 1a sin('0a' '10u' '20k' '0s') 
-c1 vin vout 10p
+xi0 gnd vin n0 n1 vout lf347 
 r0 vin vout 400k noisy=0
+c1 vin vout 10p
 
 
 
@@ -16,7 +16,7 @@ r0 vin vout 400k noisy=0
 .include '../../../SpiceModels/ECE214_models.mod'
 .include '../../../SpiceModels/ECE342_models.mod'
 .temp 27
-.ac dec 200 0 1e6
+.ac dec 200 0 1e7
 .global gnd
 .options post=1 delmax=5p relv=1e-6 reli=1e-6 relmos=1e-6 method=gear
 
